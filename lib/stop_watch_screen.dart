b54ea65496d121483d0698ba18a555f83cde79cd
Ayaan <onestar7984@gmail.com>
2023-12-20 21:49:49 +0900

    feat: StopWatch 기능 구현

--- lib/stop_watch_screen.dart
+++ lib/stop_watch_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 class StopWatchScreen extends StatefulWidget {
@@ -8,8 +10,57 @@ class StopWatchScreen extends StatefulWidget {
 }
 
 class _StopWatchScreenState extends State<StopWatchScreen> {
+  Timer? _timer;
+  int _time = 0;
+  bool _isRunning = false;
+  final _lapTimes = <String>[];
+
+  void _tappedRunningButton() {
+    _isRunning = !_isRunning;
+    if (_isRunning) {
+      _start();
+    } else {
+      _pause();
+    }
+  }
+
+  void _start() {
+    _time = 0;
+    _timer = Timer.periodic(const Duration(milliseconds: 10), (timer) {
+      setState(() {
+        _time++;
+      });
+    });
+  }
+
+  void _pause() {
+    _timer?.cancel();
+  }
+
+  void _reset() {
+    setState(() {
+      _isRunning = false;
+      _timer?.cancel();
+      _lapTimes.clear();
+      _time = 0;
+    });
+  }
+
+  void _recordLapTime(String time) {
+    _lapTimes.insert(0, time);
+  }
+
+  @override
+  void dispose() {
+    _timer?.cancel();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
+    int sec = _time ~/ 100;
+    String hundredth = '${_time % 100}'.padLeft(2, '0');
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('스톱워치'),
@@ -19,16 +70,16 @@ class _StopWatchScreenState extends State<StopWatchScreen> {
           const SizedBox(
             height: 30,
           ),
-          const Row(
+          Row(
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.end,
             children: [
               Text(
-                '0',
-                style: TextStyle(fontSize: 50),
+                '$sec',
+                style: const TextStyle(fontSize: 50),
               ),
               Text(
-                '00',
+                hundredth,
               ),
             ],
           ),
@@ -36,14 +87,7 @@ class _StopWatchScreenState extends State<StopWatchScreen> {
             width: 100,
             height: 200,
             child: ListView(
-              children: [
-                const Text('1111'),
-                const Text('1111'),
-                const Text('1111'),
-                const Text('1111'),
-                const Text('1111'),
-                const Text('1111'),
-              ],
+              children: _lapTimes.map((e) => Center(child: Text(e))).toList(),
             ),
           ),
           const Spacer(),
@@ -52,17 +96,25 @@ class _StopWatchScreenState extends State<StopWatchScreen> {
             children: [
               FloatingActionButton(
                 backgroundColor: Colors.orange,
-                onPressed: () {},
+                onPressed: () => _reset(),
                 child: const Icon(Icons.refresh),
               ),
               FloatingActionButton(
                 backgroundColor: Colors.blue,
-                onPressed: () {},
-                child: const Icon(Icons.play_arrow),
+                onPressed: () {
+                  setState(() {
+                    _tappedRunningButton();
+                  });
+                },
+                child: _isRunning ? const Icon(Icons.pause) : const Icon(Icons.play_arrow),
               ),
               FloatingActionButton(
                 backgroundColor: Colors.green,
-                onPressed: () {},
+                onPressed: () {
+                  setState(() {
+                    _recordLapTime('$sec.$hundredth');
+                  });
+                },
                 child: const Icon(Icons.add),
               ),
             ],
