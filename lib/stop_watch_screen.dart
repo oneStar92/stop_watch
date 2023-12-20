ef5bcfa8b543226c47404a86826277996527d5b1
Ayaan <onestar7984@gmail.com>
2023-12-20 21:20:22 +0900

    refactor: StopWatchScreen 간단하게 화면 구현

--- lib/stop_watch_screen.dart
+++ lib/stop_watch_screen.dart
@@ -11,6 +11,67 @@ class _StopWatchScreenState extends State<StopWatchScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('스톱워치'),
+      ),
+      body: Column(
+        children: [
+          const SizedBox(
+            height: 30,
+          ),
+          const Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Text(
+                '0',
+                style: TextStyle(fontSize: 50),
+              ),
+              Text(
+                '00',
+              ),
+            ],
+          ),
+          SizedBox(
+            width: 100,
+            height: 200,
+            child: ListView(
+              children: [
+                const Text('1111'),
+                const Text('1111'),
+                const Text('1111'),
+                const Text('1111'),
+                const Text('1111'),
+                const Text('1111'),
+              ],
+            ),
+          ),
+          const Spacer(),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              FloatingActionButton(
+                backgroundColor: Colors.orange,
+                onPressed: () {},
+                child: const Icon(Icons.refresh),
+              ),
+              FloatingActionButton(
+                backgroundColor: Colors.blue,
+                onPressed: () {},
+                child: const Icon(Icons.play_arrow),
+              ),
+              FloatingActionButton(
+                backgroundColor: Colors.green,
+                onPressed: () {},
+                child: const Icon(Icons.add),
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+        ],
+      ),
     );
   }
 }
